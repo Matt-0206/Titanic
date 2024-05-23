491bac68e914695e4af37382e96f7d40c21ca266
Matteo_Alliu <malliu431@gmail.com>
2024-05-23 11:25:54 +0200

    23/05/2024

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function populate(){
 
-    const requestURL = "https://matt-0206.github.io/Titanic/Json/titanic.json"; // gli viene assegnato un link 
+    const requestURL = "https://matt-0206.github.io/Titanic/titanic.json"; // gli viene assegnato un link 
 
     const request = new Request(requestURL) // crea un nuovo oggetto
 
@@ -14,19 +14,49 @@ async function populate(){
 
 function populateCards(obj){
 
-const container = document.getElementById('card');
+const container = document.getElementById('cards');
 
-const passeggeri = obj.passeggeri;
+const passeggeri = obj.passengers;
 
 for(const passeggero of passeggeri){
 
-    const mypara1 = document.createElement('p');
-    mypara1.textContent = passeggero.Name;
+    const card = document.createElement('div');
 
-    container.appendChild(mypara1);
+    const mypara1 = document.createElement('h3');
+    mypara1.textContent = `Name: ${passeggero.Name}`;
+
+    const mypara2 = document.createElement('p');
+    mypara2.textContent = `Sex: ${passeggero.Sex}`;
+
+    const mypara3 = document.createElement('p');
+    mypara3.textContent = `Age: ${passeggero.Age}`;
+
+    const mypara4 = document.createElement('p');
+    mypara4.textContent = `Embarked: ${passeggero.Embarked}`;
+
+    const mypara5 = document.createElement('p');
+    mypara5.textContent = `Class: ${passeggero.Pclass}`;
+
+    const mypara6 = document.createElement('p');
+    mypara6.textContent = `Survived: ${passeggero.Survived}`;
+
+    container.appendChild(card);
+
+    card.appendChild(mypara1);
+    card.appendChild(mypara2);
+    card.appendChild(mypara3);
+    card.appendChild(mypara4);
+    card.appendChild(mypara5);
+    card.appendChild(mypara6);
 
 }   
 
 }
 
 populate();
+
+/* --- LightBox --- */ 
+
+
+
+/* --------- */
